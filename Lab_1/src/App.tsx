77176fbebdbae517a2d7433dac1e25be0f9c2099
Christian Jucutan <cjucutan@rrc.ca>
2025-09-04 13:27:17 -0500

    Feat: Created a json file that lists all employees

--- Lab_1/src/App.tsx
+++ Lab_1/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Nav from './components/common/Nav/nav'
 import Footer from './components/common/Footer/footer'
 
 function App() {
   return (
-    <Footer />
+    <>
+      <Nav />
+      <Footer />
+    </>
   )
 }
 
